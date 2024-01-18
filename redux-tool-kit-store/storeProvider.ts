b5fd4097288c037edd7dbcae33987c8f20bdc44b
kashanghori <kashan.ghori@esipick.com>
2024-01-18 20:32:06 +0500

    added react redux snippets

---
+++ redux-tool-kit-store/storeProvider.ts
@@ -0,0 +1,20 @@
+'use client'
+import { useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore, AppStore } from './store'
+
+export default function StoreProvider({
+  count,
+  children,
+}: {
+  count: number
+  children: React.ReactNode
+}) {
+  const storeRef = useRef<AppStore | null>(null)
+  if (!storeRef.current) {
+    storeRef.current = makeStore()
+    // storeRef.current.dispatch(action(payload))
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>
+}
